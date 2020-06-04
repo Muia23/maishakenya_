319677d7ee6768ed6d60c99aaf25b9332d1fd5cc
alishakavene <akmunywoki@gmail.com>
2020-06-04 19:29:07 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+jquery(document).ready(function(){
+  $('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+});
+$('#recipeCarousel').carousel({
+  interval: 10000
+})
+
+$('.carousel .carousel-item').each(function(){
+    var minPerSlide = 3;
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i=0;i<minPerSlide;i++) {
+        next=next.next();
+        if (!next.length) {
+        	next = $(this).siblings(':first');
+      	}
+
+        next.children(':first-child').clone().appendTo($(this));
+      }
+});
